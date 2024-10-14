d4cb991bab34a6c3376591ad3fa2c4127a729d34
sparku7 <sparku7@sky.com>
2024-10-14 15:21:09 +0100

    Awards icons added.

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../css/Rewards.css'; // Make sure to create this CSS file for styling
 
+
 const achievements = [
-  { id: 1, name: '£1,000 Saved', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 2, name: '£5,000 Saved', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 3, name: '£10,000 Saved', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 4, name: '4 Weeks Within Budget', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 5, name: '8 Weeks Within Budget', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 6, name: 'Emergency Fund Established', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 7, name: 'Debt-Free Milestone', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 8, name: 'Save for a Vacation', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 9, name: 'Save for a New Gadget', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
-  { id: 10, name: 'Refer a Friend', imageUrl: 'https://via.placeholder.com/150', completion: '0%' },
+    { id: 1, name: '£1,000 Saved', imageUrl: require('../images/minecraft-award1.png'), completion: '0%' },
+  { id: 2, name: '£5,000 Saved', imageUrl: require('../images/minecraft-award2.png'), completion: '0%' },
+  { id: 3, name: '£10,000 Saved', imageUrl: require('../images/minecraft-award3.png'), completion: '0%' },
+  { id: 4, name: '4 Weeks Within Budget', imageUrl:require('../images/minecraft-award4.png'), completion: '0%' },
+  { id: 5, name: '8 Weeks Within Budget', imageUrl: require('../images/minecraft-award5.png'),completion: '0%' },
+  { id: 6, name: 'Emergency Fund Established', imageUrl: require('../images/minecraft-award6.png'), completion: '0%' },
+  { id: 7, name: 'Debt-Free Milestone', imageUrl: require('../images/minecraft-award7.png'), completion: '0%' },
+  { id: 8, name: 'Save for a Vacation', imageUrl: require('../images/minecraft-award8.png'),completion: '0%' },
+  { id: 9, name: 'Save for a New Gadget', imageUrl:require('../images/minecraft-award9.png'), completion: '0%' },
+  { id: 10, name: 'Refer a Friend', imageUrl: require('../images/minecraft-award10.png'), completion: '0%' },
 ];
 
 const AchievementsPage = () => {
